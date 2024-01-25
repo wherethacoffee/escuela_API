144fd9f7fc62ab45eae2ae04b51175d6d5210486
wherethacoffee <whr.thacoff23@gmail.com>
2024-01-25 14:11:48 -0600

    project initialized

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	app := fiber.New()
+
+	app.Listen(":3000")
+	fmt.Println("Server on port: 3000")
 }
